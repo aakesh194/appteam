26590605a598c785923308d0bc554f1837bb6c5b
aakesh194 <aakeshyoga123@gmail.com>
2025-10-04 19:56:11 -0400

    Week 3 + Finished Project 2

---
+++ appteam/_modules/meeting 3/week_threeView.swift
@@ -0,0 +1,18 @@
+//
+//  week_threeView.swift
+//  appteam
+//
+//  Created by aakesh y on 9/30/25.
+//
+
+import SwiftUI
+
+struct week_threeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    week_threeView()
+}
